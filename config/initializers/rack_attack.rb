15b0652ebeb1b59cc5d486088b9821b80ea26ac8
Armand Fardeau <armandfardeau@users.noreply.github.com>
2022-04-25 07:15:11 +0200

    Update rack_attack.rb

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
-if Rails.env.production?
+if Rails.env.production? || Rails.env.test?
+  require "rack/attack"
+
   class Rack::Attack
-    throttle("req/ip", limit: Decidim.throttling_max_requests, period: Decidim.throttling_max_requests) do |req|
+    throttle("req/ip", limit: 100, period: 1.minute) do |req|
       Rails.logger.warn("[Rack::Attack] [THROTTLE - req / ip] :: #{req.ip} :: #{req.path} :: #{req.GET}")
       req.ip unless req.path.start_with?("/assets")
     end
